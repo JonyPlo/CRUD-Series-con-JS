84691245100f8960aae4e9865923317efc514d2f
JonyPlo <joonyyplo@gmail.com>
2022-07-05 10:58:43 -0300

    Se agrego la clase Serie

---
+++ js/serieClass.js
@@ -0,0 +1,10 @@
+export class Serie {
+    constructor(codigo, titulo, descripcion, urlImg, genero) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.urlImg = urlImg;
+        this.genero = genero;
+    }
+    // Agregar metodos necesarios
+}
